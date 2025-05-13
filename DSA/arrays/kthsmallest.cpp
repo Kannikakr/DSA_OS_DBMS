1612878020d34f24895c719763013f117a7bb17c
kannikakr <kannikashivu18@gmail.com>
2025-05-13 20:25:04 +0530

    addedkth smallest

--- DSA/arrays/kthsmallest.cpp
+++ DSA/arrays/kthsmallest.cpp
@@ -3,3 +3,27 @@ k is smaller than the size of the array,
 the task is to find the kth smallest element 
 in the given array.*/
 
+class Solution {
+    public:
+      // arr : given array
+      // k : find kth smallest element and return using this function
+      int kthSmallest(vector<int> &arr, int k) {
+          int n = arr.size();
+          for (int i = 0; i < k; i++) {
+          int minIndex = i;
+  
+          for (int j = i+1; j < n; j++) {
+              if (arr[j] < arr[minIndex]) {
+                  minIndex = j;
+              }
+          }
+  
+          swap(arr[i], arr[minIndex]);
+      }
+      return arr[k - 1];
+          
+      }
+  };
+
+// TC = O(k*n)
+// SC = O(1)
